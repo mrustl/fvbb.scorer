7185f128388e44400dbedbe237266874064b6186
mrustl <michael.rustler@kompetenz-wasser.de>
2020-01-30 09:11:53 +0100

    add team infos and remove "forfait" games

--- R/get_scorers.R
+++ R/get_scorers.R
@@ -1,3 +1,74 @@
+#' Get Teams
+#'
+#' @param game_link_full link to game stats url
+#' @return table with team players
+#' @export
+#'
+#' @examples
+#' games_u11 <- get_links_game()[7,]
+#' completed_games_u11 <- urls_completed_games(games_u11$links_url)
+#' if(length(completed_games_u11)>0) get_teams(completed_games_u11[1]) 
+get_teams <- function(game_link_full) {
+  
+  game_master_wide <- get_game_master(game_link_full)
+  
+  team_names <- xml2::read_html(game_link_full) %>% rvest::html_nodes("b")
+  
+  team_names <- stringr::str_remove_all(team_names[!is.na(team_names %>% rvest::html_attr("style"))] %>% rvest::html_text(), 
+                                        ":")
+  
+  players <- xml2::read_html(game_link_full) %>% rvest::html_nodes("div") %>% rvest::html_attr("style") 
+  team_idx <- grepl("^width", players)
+  
+  teams <- xml2::read_html(game_link_full) %>% rvest::html_nodes("div") 
+  teams <- teams[team_idx] %>% rvest::html_text()
+  
+ team_1 <- tibble::tibble(team = team_names[1], 
+                 name = unlist(strsplit(teams[1],split = ", ")),
+                 type = dplyr::if_else(grepl("\\[C\\]", .data$name), "captain", "player")) %>%  
+    dplyr::mutate(name = stringr::str_remove_all(.data$name, "\\[C\\]") %>% stringr::str_trim())
+ 
+ team_2 <- tibble::tibble(team = team_names[2], 
+                name = unlist(strsplit(teams[2],split = ", ")),
+                type = dplyr::if_else(grepl("\\[C\\]", .data$name), "captain", "player")) %>%  
+   dplyr::mutate(name = stringr::str_remove_all(.data$name, "\\[C\\]") %>% stringr::str_trim())
+ 
+   cbind(game_master_wide[,"Spielnummer", drop = FALSE], 
+         dplyr::bind_rows(team_1, team_2)) %>% tibble::as.tibble()
+  
+}
+
+#' Get Game Master Data
+#'
+#' @param game_link_full link to game stats url
+#' @return table with game master data
+#' @export
+#' @examples
+#' games_u11 <- get_links_game()[7,]
+#' completed_games_u11 <- urls_completed_games(games_u11$links_url)
+#' if(length(completed_games_u11)>0) get_game_master(completed_games_u11[1]) 
+get_game_master <- function(game_link_full) {
+  
+  game_tables <- xml2::read_html(game_link_full) %>% rvest::html_table()
+  
+  
+  
+  game_master <- game_tables[[1]]
+  
+  colnames(game_master) <- c("key", "value")
+  
+  game_master$key <- gsub(pattern = ":", "", game_master$key)
+  game_master <- game_master[game_master$key != "", ]
+  
+  game_master_wide <- tidyr::spread(data = game_master, key = "key", value = "value")
+  
+  game_master_wide$Spielnummer <- as.numeric(game_master_wide$Spielnummer)
+  game_master_wide$Zuschauerzahl <- as.numeric(game_master_wide$Zuschauerzahl)
+  
+  game_master_wide
+}
+
+
 #' Get Scorers
 #'
 #' @param game_link_full link to game stats url
@@ -17,21 +88,10 @@
 #' 
 get_scorers <- function(game_link_full) {
 
+game_master_wide <- get_game_master(game_link_full)
+
 game_tables <- xml2::read_html(game_link_full) %>% rvest::html_table()
-
-
-game_master <- game_tables[[1]]
-
-colnames(game_master) <- c("key", "value")
-
-game_master$key <- gsub(pattern = ":", "", game_master$key)
-game_master <- game_master[game_master$key != "", ]
-
-game_master_wide <- tidyr::spread(data = game_master, key = "key", value = "value")
-
-game_master_wide$Spielnummer <- as.numeric(game_master_wide$Spielnummer)
-game_master_wide$Zuschauerzahl <- as.numeric(game_master_wide$Zuschauerzahl)
-
+  
 game_stats <- game_tables[[2]]
 
 names(game_stats) <- c("time", "event", "score", "scorer", "team")
@@ -75,7 +135,13 @@ create_table_scorers <- function(url_games) {
   completed_games <- urls_completed_games(url_games)
   
   if(length(completed_games)>0) {
-   
+
+
+    team_players <- data.table::rbindlist(lapply(completed_games, function(url) get_teams(url)))
+    team_players_stats <- team_players %>%  
+      dplyr::count(team, name)  %>% 
+      dplyr::rename(scorer_name = name, games_played = n)
+       
   table_scorers <- data.table::rbindlist(lapply(completed_games, function(x) get_scorers(x)))
 
 
--- R/urls_completed_games.R
+++ R/urls_completed_games.R
@@ -14,7 +14,9 @@ urls_completed_games <- function(url_games) {
   game_results_links <- xml2::read_html(url_games) %>% 
   rvest::html_nodes("td.center a")
 
-is_over <- rvest::html_text(game_results_links) != ""
+results_text <- rvest::html_text(game_results_links)  
+  
+is_over <-  results_text  != "" & stringr::str_detect(results_text,pattern = "forfait", negate = TRUE)
 
 game_links <- game_results_links[is_over] %>%  rvest::html_attr("href") 
 game_links_full <- sprintf("%s%s", default_saisonmanager_url(), game_links)
